dbc925fc96aeab1d6b3604c147263da81480ef9f
Max Eliseev <ralvke@gmail.com>
2019-02-19 15:35:36 +0500

    Responsive

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,21 @@
 module.exports = {
   siteMetadata: {
     title: "Max Eliseev",
-    description: "",
+    description: "Max Eliseev site",
     author: "@ralkve"
   },
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-react-svg",
-    "gatsby-mdx"
+    "gatsby-mdx",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Max Eliseev site",
+        short_name: "Max Eliseev",
+        background_color: "#282323",
+        theme_color: "#282323"
+      }
+    }
   ]
 }
